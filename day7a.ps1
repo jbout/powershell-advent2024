5f1b39f3881af6622e69fc24276a032e8375d1ed
Joel Bout <joel@bout.lu>
2024-12-20 16:57:33 +0100

    Add files via upload

---
+++ day7a.ps1
@@ -0,0 +1,36 @@
+﻿[String[]]$data = Get-Content .\day7.txt
+
+$sum = 0
+foreach ($line in $data) {
+    $part = $line.split(":")
+    $numPart = $part[1].split(" ")
+    $result = $part[0]
+    $numbers = $numPart[1..$($numPart.Count-1)]
+    $posibilities = [math]::Pow(2,($numbers.count - 1))
+    $found = $false;
+    for ($i = 0; $i -lt $posibilities; $i++) {
+        [long]$calc = $numbers[0]
+        for ($pos = 1; $pos -lt $numbers.Count; $pos++) {
+            $mask = [math]::Pow(2,($pos -1))
+            [int]$op = ($i -band $mask) / $mask
+            switch ($op) {
+                0 {
+                    $calc = $calc + $numbers[$pos]
+                }
+                1 {
+                    $calc = $calc * $numbers[$pos]
+                }
+                default {
+                    echo "$op not found"
+                }
+            }
+            
+        }
+        if ($calc -eq $result) {
+            $found = $true
+            $sum += $result
+            break
+        }
+    }
+}
+echo "Sum: $sum"
